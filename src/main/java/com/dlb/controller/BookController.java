7904151e91fb5f373dc20738bcac6f11e95edb9c
TearsSZ <1216180134@qq.com>
2021-12-11 21:36:12 +0800

    66666

--- src/main/java/com/dlb/controller/BookController.java
+++ src/main/java/com/dlb/controller/BookController.java
@@ -3,5 +3,6 @@ package com.dlb.controller;
 public class BookController {
     public static void main(String[] args) {
         System.out.println("aaaa");
+        System.out.println("bbbb");
     }
 }
